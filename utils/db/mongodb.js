45336cd3a465b5c38759ea99299ce742fb54001a
bilal <mohdbilalpersonal@gmail.com>
2026-01-14 01:05:43 -0500

    Setting up MongoDb

---
+++ utils/db/mongodb.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+if (!MONGODB_URI) {
+  throw new Error("NO MONGO DB STRING FOUND");
+}
+
+async function connectDB() {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
+}
+
+export default connectDB;
