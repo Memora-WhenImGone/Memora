d1c6b433b1ca5d87e627db8b134976db765db765
soni <soni@gmail.com>
2026-02-17 17:55:33 -0500

    worked on header and cards

--- app/dash-board/page.jsx
+++ app/dash-board/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "@/components/HomePage/Header";
 import SideBar from '../../components/dashboard/SideBar';
+import MainHero from '../../components/dashboard/MainHero';
 const page = () => {
 
   return (
-  <div>
+  <div className='flex flex-row'>
     <SideBar></SideBar>
+    <MainHero></MainHero>
   </div>
   )
 }
--- components/dashboard/MainHero.jsx
+++ components/dashboard/MainHero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Header from './Header'
+import Cards from './Cards'
+import { Clock3, FolderLock, Shield, Users } from 'lucide-react'
 
 const MainHero = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col'>
+      <Header></Header>
+   <div className='flex flex-col md:flex-row'>
+  <Cards title={"Vault Status"} body={3 } icon={<Shield/>}/>
+    <Cards title={"Vault Items"} body={3 } icon={<FolderLock/>}/>
+      <Cards title={"Trusted Contracts"} body={3 } icon={<Users/>}/>
+        <Cards title={"Active Triggers"} body={3 } icon={<Clock3/>}/>
+   </div>
     </div>
   )
 }
