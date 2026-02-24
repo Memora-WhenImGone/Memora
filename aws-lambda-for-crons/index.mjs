000249f68ae1a0dee5c2dc037b3aeec36cad804c
bilal <mohdbilalpersonal@gmail.com>
2026-02-24 02:16:29 -0500

    aws lambda function

---
+++ aws-lambda-for-crons/index.mjs
@@ -0,0 +1,13 @@
+
+
+// This is an aws lambda function which is connected to event bridge 
+// that will run this function everyday 
+
+// so we will write the logic to scan the mongodb colelctions
+// apply filters on lastactivty and send triggers 
+
+
+
+export async function handler() {
+
+}
