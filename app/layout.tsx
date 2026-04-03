eed30f5042119b007249da7b05645f0a3eba0665
bilal <mohdbilalpersonal@gmail.com>
2026-04-03 15:57:29 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Memora",
   description: "When I am gone",
+   icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
