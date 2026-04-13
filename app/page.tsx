f0c9af7a53d0a0454c6a1ed145b19ea18cd69983
bilal <mohdbilalpersonal@gmail.com>
2026-04-13 01:32:23 -0400

    Team Section Removed

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default async function Page() {
       
       {/* <DonationModal /> */}
       <FAQ />
-      <TeamSection />
+      {/* <TeamSection /> */}
       <Footer />
     </main>
   );
